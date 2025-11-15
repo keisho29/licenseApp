ab801a43d7c561e97cf50986da862ed4ee6862ab
Keish <keish@example.com>
2025-11-15 23:28:08 +0900

    初期プッシュ

---
+++ src/main/java/com/example/licenseApp/LicenseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.licenseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/licenseApp/LicenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.licenseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
